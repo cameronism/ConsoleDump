29988fa4b16341820c5bca3dcbc0051a4d10c2a4
Cameron Jordan <cjordan@cameronism.com>
2013-07-25 23:19:26 -0700

    NuGet prep

--- ConsoleDump/Properties/AssemblyInfo.cs
+++ ConsoleDump/Properties/AssemblyInfo.cs
@@ -6,9 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ConsoleDump")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".Dump() extension method to visualize your collections and objects in color at the console.")]
+
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("cameronism")]
 [assembly: AssemblyProduct("ConsoleDump")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
